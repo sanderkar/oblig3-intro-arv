37b613c1bdd34ebe57ddf153e2353632f1a8ca53
Sander Skau Karlsen <sanderkar@stud.ntnu.no>
2026-02-17 18:51:06 +0100

    textcommands gjort

--- src/main/java/edu/ntnu/idatx2003/Script.java
+++ src/main/java/edu/ntnu/idatx2003/Script.java
@@ -1,4 +1,19 @@
 package edu.ntnu.idatx2003;
 
+import java.util.List;
+
 public class Script {
+  List<TextCommand> textCommands;
+
+    public Script(List<TextCommand> textCommands) {
+        this.textCommands = textCommands;
+    }
+
+    public String execute(String text) {
+        String newText = text;
+        for (TextCommand command : textCommands) {
+            newText = command.execute(newText);
+        }
+        return newText;
+    }
 }
